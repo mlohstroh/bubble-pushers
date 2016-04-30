be2c7a9922554bd7f6d617bb36d7268f5f4a2dce
Mark Lohstroh <mlohstroh@gmail.com>
2016-04-30 14:54:42 -0500

    final commenting

--- cpu.v
+++ cpu.v
@@ -20,17 +20,24 @@ module cpu(clk, rst);
   wire canBranch;
   wire canReallyBranch;
 
+  // Full alu module
   allAlu alu(regOut1, regOut2, immValue, reg_dst, regRt, regRd, alu_src, alu_ctrl, writeRegister, result, canBranch);
 
+  // Instruction decode step
   InstrDecod decode(instruction, currentPC, reg_write, writeData, writeRegister, opcode, funct, regOut1, regOut2, regRt, regRd, immValue, jumpDest, branchDest);
 
+  // Fetching the next instruction code
   if_phase fetcher(clk, rst, branchDest, canReallyBranch, jumpDest, jumpCtrl, instruction, currentPC);
 
+  // The control module
   control ctrl(opcode, funct, mem_to_reg, mem_write, branch_ctrl, alu_ctrl, alu_src, reg_dst, reg_write, jumpCtrl);
 
+  // RAM for the CPU
   data_memory mem(result, regOut2, mem_write, readData);
+  // Branching mux for the ALU result
   mux #(32) last_mux(result, readData, mem_to_reg, writeData);
 
+  // final branch control
   bestAnd branchAnd(branch_ctrl, canBranch, canReallyBranch);
 endmodule
 
@@ -54,7 +61,9 @@ module allAlu(regOut1, regOut2, immValue, regDst, regRt, regRd, alu_src, alu_ctr
   wire canBranch;
   wire [31:0] actualSource;
 
+  // alu operations based on control
   alu alu(result, canBranch, regOut1, actualSource, alu_ctrl);
+  // Muxes that select the inputs to the ALU
   mux #(5) mux(regRt, regRd, regDst, writeRegister);
   mux #(32) other_mux(regOut2, immValue, alu_src, actualSource);
 endmodule
